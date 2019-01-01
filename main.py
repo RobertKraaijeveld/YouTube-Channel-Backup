7d9148dfad54d62d7e291b5ee2ec611b55c3618a
Robert Kraaijeveld <rkraaijeveld@outlook.com>
2019-01-01 19:24:03 +0100

    added size estimation from vid details

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import argparse
 import os
 import re
+import isodate
 
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
@@ -18,11 +19,13 @@ with open('apikey.secret', 'r') as apikeyfile:
 YOUTUBE_API = build(API_SERVICE_NAME, API_VERSION, developerKey=API_KEY)
 
 
+###############################################
+# FUNCTIONS FOR GETTING VIDEO IDS FROM CHANNEL 
+###############################################
 
 def get_channel_id_by_username(username):
     requestResult = YOUTUBE_API.channels().list(part='id',forUsername=username).execute()
     return requestResult['items'][0]['id']
-
 
 def get_videos_of_channel(channelId):
     videosIdsPerPage = 10
@@ -34,13 +37,11 @@ def get_videos_of_channel(channelId):
     else: 
         return get_video_ids_from_searchresult(firstResult)
 
-
 def get_video_ids_from_searchresult(result):
     ids = []
     for videoItem in result['items']:
         ids.append(videoItem['id']['videoId'])
     return ids
-
 
 def paginate_video_ids_of_channel(pageToken, videoIds, videosIdsPerPage):
     currentPage = YOUTUBE_API.search().list(part='id',pageToken=pageToken,type='video',maxResults=videosIdsPerPage).execute()
@@ -54,10 +55,57 @@ def paginate_video_ids_of_channel(pageToken, videoIds, videosIdsPerPage):
         return paginate_video_ids_of_channel(nextPageToken, videoIds, videosIdsPerPage)
 
 
+######################################################
+# FUNCTIONS FOR GETTING VIDEO DETAILS USING VIDEO IDS
+######################################################
+
+AVG_MBS_PER_HD_SECOND = 0.23
+AVG_MBS_PER_SD_SECOND = 0.09
+
+# Per quality type, get the avg length TODO: PAGING PLS
+def get_videos_for_ids(videosIds):
+    videoIdsAsStr = ",".join(videosIds)
+    return YOUTUBE_API.videos().list(part='snippet,contentDetails',id=videoIdsAsStr).execute()
+
+def get_videos_by_quality_type(videos):
+    # Only 2 quality types, SD or k
+    vids = {}
+    vids['hd'] = [v for v in videos if v['contentDetails']['definition']]
+    vids['sd'] = [v for v in videos if v['contentDetails']['definition'] == 'sd']
+    return vids
+
+def get_average_vid_length_per_quality_type(videosPerQualityType):
+    hdVids = videosPerQualityType['hd']
+    sdVids = videosPerQualityType['sd']
+
+    sdTotalLengths = sum(isodate.parse_duration(v['contentDetails']['duration']).total_seconds() for v in sdVids)
+    hdTotalLengths = sum(isodate.parse_duration(v['contentDetails']['duration']).total_seconds() for v in hdVids)
+
+    averageVidLengthsPerQuality = {}
+    averageVidLengthsPerQuality['hd'] = hdTotalLengths / len(hdVids)
+    averageVidLengthsPerQuality['sd'] = sdTotalLengths / len(sdVids)
+    return averageVidLengthsPerQuality
+
+def get_estimated_total_size(averageVidLengthsPerQuality, amountOfHdVids, amountOfSdVids):
+    sdVidsEstimatedSize = (amountOfSdVids * averageVidLengthsPerQuality['sd']) * AVG_MBS_PER_SD_SECOND
+    hdVidsEstimatedSize = (amountOfHdVids * averageVidLengthsPerQuality['hd']) * AVG_MBS_PER_HD_SECOND
+    return sdVidsEstimatedSize + hdVidsEstimatedSize
+
+
+
 if __name__ == '__main__':
     try:
-        testChannelId = get_channel_id_by_username('davidr64yt')
+        testChannelId = get_channel_id_by_username('romulus310')
         channelVideoIds = get_videos_of_channel(testChannelId)
-        print(channelVideoIds)
+
+        if any(channelVideoIds):
+            channelVideos = get_videos_for_ids(channelVideoIds)['items']
+            videosByQualityType = get_videos_by_quality_type(channelVideos)
+
+            averageVidLengths = get_average_vid_length_per_quality_type(videosByQualityType)
+            print(get_estimated_total_size(averageVidLengths, len(videosByQualityType['hd']), len(videosByQualityType['sd'])))
+        else:
+            print("Sorry, this channel has no videos.")
+
     except HttpError as e:
         print(e)
